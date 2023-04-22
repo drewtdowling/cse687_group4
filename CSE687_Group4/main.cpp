c43fcc3d2011acb9094c88d012e3d0df45ce8106
kenzobird <104602589+kenzobird@users.noreply.github.com>
2023-04-21 17:18:22 -0700

    set up main to work with map

--- CSE687_Group4/main.cpp
+++ CSE687_Group4/main.cpp
@@ -1,3 +1,24 @@
+////////////////////////////////////////////////////////////////////////
+// main.cpp - Workflow and Executive component for the Map-Reduce     //
+// ver 1.0                                                            //
+// Language:    C++, Visual Studio 2022                               //
+// Platform:    Windows 11                                            //
+// Application: Executive component, CSE687 - Object Oriented Design  //
+// Author:      Kenzo Bird, Drew Dowling, Andrew Reichle,             //
+//              Syracuse University                                   //
+//              ksbird@syr.edu, drdowlin@syr.edu, afreichl@syr.edu    //
+////////////////////////////////////////////////////////////////////////
+/*
+Package Operations:
+-------------------
+
+
+Maintenance History:
+--------------------
+ver 1.0 : 21 April 2023
+-initial release
+*/
+
 #include "FileManagement/FileManagement.h"
 #include "map.h"
 #include <iostream>
@@ -6,16 +27,16 @@
 
 int main()
 {
+
   FileManagement fileManagement = FileManagement();
+
   Map map = Map();
-  std::stringstream test;
+  std::stringstream map_stream;
   string line = "";
-  //std::stringstream interm;
 
-  test << fileManagement.readInputFileToString("AsYouLikeIte.txt");
-
-  while (getline(test, line, '\n')) {
-    //interm << map.map(line);
+  map_stream << fileManagement.readInputFileToString("AsYouLIkeIte.txt");
+  while (getline(map_stream, line, '\n')) {
+    //std::cout << map.map(line);
     fileManagement.writeToIntermediateDirectoryWithString(map.map(line));
   }
 
